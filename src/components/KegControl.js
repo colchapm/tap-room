b32ed34a962ae17a5e2f68d5c3049d38b2b8e400
Collin Chapman <cchap14@gmail.com>
2021-08-21 21:01:34 -0400

    add KegControl component and local state

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import NewKegForm from './NewKegForm';
+import KegList from './KegList';
 
 
 class KegControl extends React.Component {
@@ -9,5 +11,32 @@ class KegControl extends React.Component {
     };
   }
 
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm />;
+      buttonText = "Return to Keg List";
+    } else {
+      currentlyVisibleState = <KegList />;
+      buttonText = "Add Keg";
+      
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+
   
 }
+
+export default KegControl;
---
+++ src/components/NewKegForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NewKegForm(props){
+  return (
+    <React.Fragment>
+      <h3>placeholder form</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewKegForm;
+
