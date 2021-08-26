53a215143c8fde51eee3fb53e4a45bca8b315179
Collin Chapman <cchap14@gmail.com>
2021-08-25 20:23:32 -0700

    Updates

---
+++ precache-manifest.db68b2d1686307d4237a8cedf7bb8148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5337944d8d3e383e684719264c1caf32",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "9dc841fe0196f564dc6a",
+    "url": "/tap-room/static/css/main.b2405eeb.chunk.css"
+  },
+  {
+    "revision": "c1571ccd6eacc2f3f1ec",
+    "url": "/tap-room/static/js/2.99fb60df.chunk.js"
+  },
+  {
+    "revision": "d9962b85509a5df882f7",
+    "url": "/tap-room/static/js/3.3270ef3c.chunk.js"
+  },
+  {
+    "revision": "9dc841fe0196f564dc6a",
+    "url": "/tap-room/static/js/main.9a2280a1.chunk.js"
+  },
+  {
+    "revision": "077965c5de951fce998a",
+    "url": "/tap-room/static/js/runtime-main.91e69f6f.js"
+  }
+]);
